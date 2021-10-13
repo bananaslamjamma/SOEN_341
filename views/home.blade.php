ab22c95f4d09ca17d47939e0ac2ae5ad95c24d75
AshiswhoIam <71729682+AshiswhoIam@users.noreply.github.com>
2021-10-13 18:25:29 -0400

    Add files via upload

---
+++ views/home.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title> Soen 341 homepage </title>
+<meta charset="UTF-8"/>
+<link rel="stylesheet" href="/css/main.css">
+
+</head>
+
+
+<body>
+<header>
+
+<div class="content">
+<h2 class="left">Soen OverFlow</h2>
+
+
+<nav>
+<ul>
+
+<li><a href=""> Home</a></li>
+<li><a href=""> Forum</a></li>
+<li><a href=""> Team Info</a></li>
+<li><a href=""> login</a></li>
+<li><a href=""> Profile</a></li>
+
+</ul>
+</nav>
+
+
+</div>
+
+</header>
+
+
+</body>
+</html>
