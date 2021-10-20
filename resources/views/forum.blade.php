9d3b5fc5aa3b1aaf10afb4b38755ef8e36baca06
pot-ui <evan.yu6748@gmail.com>
2021-10-20 19:29:56 -0400

    auth + login logouts set up
    able to register, login

--- resources/views/forum.blade.php
+++ resources/views/forum.blade.php
@@ -16,16 +16,49 @@
 
 
 <nav>
-<ul>
+                <ul>
 
-<li><a href="/home"> Home</a></li>
-<li><a href="/forum"> Forum</a></li>
-<li><a href=""> Team Info</a></li>
-<li><a href="/login"> login</a></li>
-<li><a href="/profile"> Profile</a></li>
+                    <li><a href="/home"> Home</a></li>
+                    <li><a href="/forum"> Forum</a></li>
+                    <li><a href=""> Team Info</a></li>
+                    <!-- Right Side Of Navbar -->
 
-</ul>
-</nav>
+                    <!-- Authentication Links -->
+                    @guest
+                    @if (Route::has('login'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                    @endif
+                    @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    @endguest
+
+                    <li><a href="/profile"> Profile</a></li>
+
+                </ul>
+            </nav>
 
 
 </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,37 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<title> Soen 341 homepage </title>
-<meta charset="UTF-8"/>
-<link rel="stylesheet" href="/css/main.css">
+    <title> Soen 341 homepage </title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="/css/main.css">
 
 </head>
 
 
 <body>
-<header>
+    <header>
 
-<div class="content">
-<h2 class="left">Soen OverFlow</h2>
-
-
-<nav>
-<ul>
-
-<li><a href="/home"> Home</a></li>
-<li><a href="/forum"> Forum</a></li>
-<li><a href=""> Team Info</a></li>
-<li><a href="/login"> login</a></li>
-<li><a href="/profile"> Profile</a></li>
-
-</ul>
-</nav>
+        <div class="content">
+            <h2 class="left">Soen OverFlow</h2>
 
 
-</div>
+            <nav>
+                <ul>
 
-</header>
+                    <li><a href="/home"> Home</a></li>
+                    <li><a href="/forum"> Forum</a></li>
+                    <li><a href=""> Team Info</a></li>
+                    <!-- Right Side Of Navbar -->
+
+                    <!-- Authentication Links -->
+                    @guest
+                    @if (Route::has('login'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    </li>
+                    @endif
+                    @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    @endguest
+
+                    <li><a href="/profile"> Profile</a></li>
+
+                </ul>
+            </nav>
+
+
+        </div>
+
+    </header>
 
 
 </body>
+
 </html>
